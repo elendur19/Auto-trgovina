d02b021a20d6f1f9640db130160b2eb0526fb56e
Shimun Matic <sm@maxviva.com>
2021-05-16 23:06:11 +0200

    added cross origin for localhost

--- src/main/java/hr/fer/ris/autotrgovina/controller/VehicleController.java
+++ src/main/java/hr/fer/ris/autotrgovina/controller/VehicleController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/vehicle")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class VehicleController {
     private final VehicleService vehicleService;
 
