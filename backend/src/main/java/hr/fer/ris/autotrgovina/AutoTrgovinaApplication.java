eb8ef6daaf75211700931dd1bf39097bf649ad05
elendur19 <mislav.robic@gmail.com>
2021-10-17 17:21:01 +0200

    Added User Roles

--- backend/src/main/java/hr/fer/ris/autotrgovina/AutoTrgovinaApplication.java
+++ backend/src/main/java/hr/fer/ris/autotrgovina/AutoTrgovinaApplication.java
@@ -3,7 +3,7 @@ package hr.fer.ris.autotrgovina;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class AutoTrgovinaApplication {
 
     public static void main(String[] args) {
